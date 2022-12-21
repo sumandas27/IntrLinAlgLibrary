7e97f3e6fb95b6dfc18eb50b0b563fdadac2dbd6
sumandas27 <sumanta.das2727@gmail.com>
2022-12-21 02:16:05 -0500

    fixed typos

--- main.cpp
+++ main.cpp
@@ -2,11 +2,13 @@
 
 int main(int argc, char** argv) {
     
+    /* TEST CODE */
+
     using ila::Vector, ila::Matrix;
 
     std::vector<Vector<3>> mySet =
     {
-        Vector<3>(1, 2, 3),
+        Vector<3>(1000, 2, 3),
         Vector<3>(4, 5, 6)
     };
 
